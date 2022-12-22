a76bad45b507c1157559a9f41bea50b2ce7baba0
Heniinmay <realhayley05@gmail.com>
2022-12-22 13:19:27 +0900

    section0 : first commit

---
+++ section0.js
@@ -0,0 +1,19 @@
+/** 0.1 변수란? */
+// 프로그래밍 is 데이터 조작
+// 데이터를 담아 두는 상자와 같은 개념 ; 변수
+
+let box = 123 * 123;
+console.log(box);
+
+let word = "abc";
+let num = 123;
+
+console.log(word, num); // return abc, 123
+
+/** 0.2 변수 선언과 데이터 할당 */
+//할당 연산자 ' = '
+//! 같다가 아니다.
+
+/** 0.3 변수 선언 키워드 */
+
+/** 0.4 String, Number */
