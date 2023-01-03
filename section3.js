2cb7d017740faf94f14d3cf1357dc7d8b3391294
Heniinmay <realhayley05@gmail.com>
2023-01-04 00:14:42 +0900

    section3.5 : 비교연산자

---
+++ section3.js
@@ -0,0 +1,12 @@
+//3.4 비교 연산자
+console.log("1 === 1", 1 === 1);
+console.log(1 > 2); //false
+
+const check = "check";
+console.log("check === 'check'", check === "check");
+console.log("check === 123", check === 123);
+
+const arr = [1, 2, 3];
+console.log("arr === [1,2,3]", arr === [1, 2, 3]); //???
+
+//3.5 배열과 객체의 비교
