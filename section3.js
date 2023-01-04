e04bc5f55206c06e214a40bc122c4719f56e91bb
Heniinmay <realhayley05@gmail.com>
2023-01-05 01:21:59 +0900

    section3

--- section3.js
+++ section3.js
@@ -9,4 +9,20 @@ console.log("check === 123", check === 123);
 const arr = [1, 2, 3];
 console.log("arr === [1,2,3]", arr === [1, 2, 3]); //???
 
-//3.5 배열과 객체의 비교
+//3.5 배열과 객체의 비교 (Notion)
+
+//3.6 조건문과 논리 연산자 (if문)
+const obj = {
+  name: "Peter",
+  age: 25,
+};
+if (obj.name === "Jason" || "Peter") {
+  console.log("Hi, " + obj.name);
+} else {
+  console.log("not member");
+}
+
+/**
+ * && AND 연산자
+ * || OR 연산자
+ */
